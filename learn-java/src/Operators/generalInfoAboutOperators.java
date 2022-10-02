ce8c24aff7055b365ec5bfcfbdea8ed5b7bb871d
Inam10 <87555875+Inam10@users.noreply.github.com>
2022-10-02 00:11:34 -0700

    operators

---
+++ learn-java/src/Operators/generalInfoAboutOperators.java
@@ -0,0 +1,28 @@
+package Operators;
+
+public class generalInfoAboutOperators {
+
+	public static void main(String[] args) {
+		//arithmatic operators
+		int a = 10 ;
+		int b = 20 ;
+		System.out.println(a+b);
+		//+,-,*,/,%
+		
+		//Unary Operators; ++ , --
+		int c = 10 ;
+		System.out.println(++c);
+		//pre increment and pre decrement operators here 
+		
+		//Relational Operators == , != ,  < , > ,  <= , >=
+		
+		//Logical operators &&, || , !
+		
+		//Binary Number system
+		
+		//Bitwise Operators
+		
+
+	}
+
+}
