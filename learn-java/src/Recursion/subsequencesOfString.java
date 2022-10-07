03f12735c4038c3f9a6c78a66a38505b5b57c963
Inam10 <87555875+Inam10@users.noreply.github.com>
2022-10-06 23:45:07 -0700

    more

--- learn-java/src/Recursion/subsequencesOfString.java
+++ learn-java/src/Recursion/subsequencesOfString.java
@@ -1,9 +1,26 @@
 package Recursion;
 
 public class subsequencesOfString {
+	
+public static void subsequences (String str , int idx , String newString) {
+	if(idx == str.length()) {
+		System.out.println(newString);
+		return;
+	}
+	
+	char currChar = str.charAt(idx);
+	
+	subsequences(str, idx+1, newString+currChar);
+	
+	subsequences(str, idx+1, newString);
+}
 
 	public static void main(String[] args) {
 	
+		String str = "abc";
+		subsequences(str, 0, "");
 	}
 
 }
+
+//lenght power of n of 2 means when your character numbers is 3 then output series is 8
